0d185a0c41bf32329c798cda7593e26be834edee
Kajto96 <kajetan1996@gmail.com>
2022-05-14 23:23:38 +0200

    space

--- chessboard.js
+++ chessboard.js
@@ -1,36 +1,24 @@
 var prevEl = null;
 var prevColor = null;
 for (let e of document.querySelectorAll("div.chessboard > div > div.black")) {
-    e.onclick = function () {
-        if (e.classList.contains('circle')){
-            
-        }
-        else {
-            e.classList.add('circle');
-            prevEl.classList.remove('circle');
-        }
-        if (prevEl === e) {      
-            if (e.style.backgroundColor === prevColor) {
-                e.style.backgroundColor = "#808080";
-                
-            } else {
-                e.style.backgroundColor = prevColor;
-                         
-            }
+  e.onclick = function () {
+      if (!e.classList.contains('circle')) {      
+         e.classList.add('circle');
+        prevEl.classList.remove('circle');
+      }
+     if (prevEl === e) {
+        if (e.style.backgroundColor === prevColor) {
+               e.style.backgroundColor = "#808080";   
         } else {
-            if (prevEl !== null) {
-                prevEl.style.backgroundColor = prevColor;   
-            }
-           
-
-            prevEl = e;
-            prevColor = e.style.backgroundColor;
-            e.style.backgroundColor = "#808080";
-            
-                
-            
-            
+            e.style.backgroundColor = prevColor;            
+         }
+      } else {
+        if (prevEl !== null) {
+               prevEl.style.backgroundColor = prevColor;   
         }
-       
+           prevEl = e;
+           prevColor = e.style.backgroundColor;
+        e.style.backgroundColor = "#808080";   
+      }    
     }
 }
