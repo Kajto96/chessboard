1720fc8dc81145dd93006d30e4998ed6bfb7e99b
Kajto96 <kajetan1996@gmail.com>
2022-05-22 16:01:02 +0200

    after eslint

--- chessboard.js
+++ chessboard.js
@@ -1,24 +1,24 @@
-var prevEl = null;
-var prevColor = null;
-for (let e of document.querySelectorAll("div.chessboard > div > div.black")) {
+let prevEl = null
+let prevColor = null
+for (const e of document.querySelectorAll('div.chessboard > div > div.black')) {
   e.onclick = function () {
-      if (!e.classList.contains('circle')) {      
-         e.classList.add('circle');
-        prevEl.classList.remove('circle');
-      }
-     if (prevEl === e) {
-        if (e.style.backgroundColor === prevColor) {
-               e.style.backgroundColor = "#808080";   
-        } else {
-            e.style.backgroundColor = prevColor;            
-         }
-      } else {
-        if (prevEl !== null) {
-               prevEl.style.backgroundColor = prevColor;   
-        }
-           prevEl = e;
-           prevColor = e.style.backgroundColor;
-        e.style.backgroundColor = "#808080";   
-      }    
+    if (!e.classList.contains('circle')) {
+      e.classList.add('circle')
+      prevEl.classList.remove('circle')
     }
+    if (prevEl === e) {
+      if (e.style.backgroundColor === prevColor) {
+        e.style.backgroundColor = '808080'
+      } else {
+        e.style.backgroundColor = prevColor
+      }
+    } else {
+      if (prevEl !== null) {
+        prevEl.style.backgroundColor = prevColor
+      }
+      prevEl = e
+      prevColor = e.style.backgroundColor
+      e.style.backgroundColor = '#808080'
+    }
+  }
 }
