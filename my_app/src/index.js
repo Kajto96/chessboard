ba714fc2c9d58e9bbfab0f394b9643a251ef1d43
Kajto96 <kajetan1996@gmail.com>
2023-10-21 18:14:05 +0200

    Removed old files

--- my_app/src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import Board from './App';
-import "./chessboard.css"
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
-root.render(<Board />);
